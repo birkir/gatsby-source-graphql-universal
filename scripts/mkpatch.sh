f80aa732a37bc41181990887e77ceaacbe2c4934
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-01-03 02:05:54 -0500

    Inherit graphql source plugin

--- scripts/mkpatch.sh
+++ scripts/mkpatch.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-mkdir -p ./tmp
-
-if [ -d "./tmp/gatsby/.git" ]; then
-  # cd ./tmp/gatsby; git pull; cd -;
-  echo "Already have git directory in ./tmp/gatsby"
+rm -rf ./tmp
+mkdir ./tmp
+if [ -d "./tmp" ]; then
+  echo "Already have directory ./tmp"
 else
   git clone --depth 1 https://github.com/gatsbyjs/gatsby.git ./tmp/gatsby
 fi
@@ -18,10 +16,10 @@ cd -
 git checkout -b feature/upstream-merge
 cp ./tmp/gatsby/packages/babel-plugin-remove-graphql-queries/src/index.js ../src/babel-plugin-remove-graphql-queries.js
 cp ./tmp/gatsby/packages/gatsby/src/utils/babel-loader.js ../src/babel-loader.js
-cp ./tmp/gatsby/packages/gatsby-source-graphql/src/gatsby-node.js ./src/gatsby-node.js
-cp ./tmp/gatsby/packages/gatsby-source-graphql/src/transforms.js ./src/transforms.js
 
 echo "Review changes and push. Otherwise:"
 echo "  git checkout master"
 echo "  git branch -D feature/upstream-merge"
 echo ""
+
+rm -rf ./tmp
--- src/utils.js
+++ src/utils.js
@@ -15,27 +15,3 @@ exports.prepareOptions = (babel) => {
 
   return items
 }
-
-exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
-  const config = getConfig()
-
-  const traverseRule = rule => {
-    if (rule.oneOf && Array.isArray(rule.oneOf)) {
-      return { ...rule, oneOf: rule.oneOf.map(traverseRule) }
-    }
-
-    if (rule.use && Array.isArray(rule.use)) {
-      rule.use.forEach(ruleUse => {
-        if (ruleUse.loader && ruleUse.loader.indexOf(`gatsby/dist/utils/babel-loader.js`) >= 0) {
-          ruleUse.loader = require.resolve(`gatsby-source-graphql-universal/babel-loader.js`)
-        }
-      })
-    }
-
-    return rule
-  }
-
-  config.module.rules = config.module.rules.map(traverseRule)
-
-  actions.replaceWebpackConfig(config)
-};

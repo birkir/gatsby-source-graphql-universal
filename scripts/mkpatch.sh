d9b198733d15b3fab905dbdbc6a8784588525a8a
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-01-03 01:51:36 -0500

    Patching 101

--- scripts/mkpatch.sh
+++ scripts/mkpatch.sh
@@ -2,9 +2,26 @@
 
 mkdir -p ./tmp
 
-# git clone --depth 1 https://github.com/gatsbyjs/gatsby.git ./tmp/gatsby
+if [ -d "./tmp/gatsby/.git" ]; then
+  # cd ./tmp/gatsby; git pull; cd -;
+  echo "Already have git directory in ./tmp/gatsby"
+else
+  git clone --depth 1 https://github.com/gatsbyjs/gatsby.git ./tmp/gatsby
+fi
 
-cd ./tmp/gatsby; git pull; cd -;
+# Apply patches
+cd ./tmp/gatsby
+git apply ../../patches/babel-plugin-remove-graphql-queries.patch -v
+git apply ../../patches/babel-loader.patch -v
+cd -
 
-git diff --no-index ./tmp/gatsby/packages/babel-plugin-remove-graphql-queries/src/index.js ../src/babel-plugin-remove-graphql-queries.js > ./tmp/babel-plugin-remove-graphql-queries.patch
-sed -i -e 's/.\/tmp\/gatsby\/packages\/babel-plugin-remove-graphql-queries\/src\/index.js/..\/src\/babel-plugin-remove-graphql-queries.js/g' ./tmp/babel-plugin-remove-graphql-queries.patch
+git checkout -b feature/upstream-merge
+cp ./tmp/gatsby/packages/babel-plugin-remove-graphql-queries/src/index.js ../src/babel-plugin-remove-graphql-queries.js
+cp ./tmp/gatsby/packages/gatsby/src/utils/babel-loader.js ../src/babel-loader.js
+cp ./tmp/gatsby/packages/gatsby-source-graphql/src/gatsby-node.js ./src/gatsby-node.js
+cp ./tmp/gatsby/packages/gatsby-source-graphql/src/transforms.js ./src/transforms.js
+
+echo "Review changes and push. Otherwise:"
+echo "  git checkout master"
+echo "  git branch -D feature/upstream-merge"
+echo ""

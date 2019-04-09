0a703a396bba43db18d9ba7eec64b8909ccb24d0
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-04-09 10:01:39 -0400

    v3.0.6

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -4,21 +4,26 @@ const { babelParseToAst } = require('gatsby/dist/utils/babel-parse-to-ast');
 const { sourceNodes } = require('gatsby-source-graphql/gatsby-node');
 
 const getRootQuery = (componentPath) => {
-  const content = fs.readFileSync(componentPath, 'utf-8');
-  const ast = babelParseToAst(content);
-  const exported = get(ast, 'program.body', []).filter(
-    (n) => n.type === 'ExportNamedDeclaration'
-  );
-  const exportedQuery = exported.find(exp =>
-    get(exp, 'declaration.declarations.0.id.name') === 'query'
-  );
+  try {
+    const content = fs.readFileSync(componentPath, 'utf-8');
+    const ast = babelParseToAst(content);
+    const exported = get(ast, 'program.body', []).filter(
+      (n) => n.type === 'ExportNamedDeclaration'
+    );
+    const exportedQuery = exported.find(exp =>
+      get(exp, 'declaration.declarations.0.id.name') === 'query'
+    );
 
-  if (exportedQuery) {
-    const query = get(exportedQuery, 'declaration.declarations.0.init.quasi.quasis.0.value.raw');
+    if (exportedQuery) {
+      const query = get(exportedQuery, 'declaration.declarations.0.init.quasi.quasis.0.value.raw');
 
-    if (query) {
-      return query;
+      if (query) {
+        return query;
+      }
     }
+  } catch (err) {
+    console.error('gatsby-source-prismic-universal: Could not parse component path: ', componentPath);
+    console.error(err);
   }
   return null;
 };

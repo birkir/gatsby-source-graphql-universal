c5fb1f447fe497c69f566e6469ff9b6ad36eb5c8
Nathan <NathHorrigan@gmail.com>
2019-07-04 17:26:46 +0100

    Fix webpack dynamic import issue on preview

--- src/preview-template.js
+++ src/preview-template.js
@@ -1,54 +1,70 @@
- /* eslint-disable */
-import React from "react"
-import {
-  decodePreviewUrl,
-  withPreview
-} from "./preview"
-import baseFragments from '../../.cache/fragments/gatsby-source-wagtail-fragments.js'
+/* eslint-disable */
+import React from "react";
+import { decodePreviewUrl, withPreview } from "./preview";
+import { query as wagtailBaseFragments } from "../../.cache/fragments/gatsby-source-wagtail-fragments.js";
 
-const PreviewPage = props => {
-    const { pageMap, fragmentFiles = [] } = props.pageContext
-    let components = {}
-    const isBrowser = typeof window != 'undefined'
-    if (pageMap && isBrowser) {
-        
-        // Import all fragment files and extract string
-        let fragments = ''
-        if (baseFragments) {
-            fragments += baseFragments.source
-        }
+class PreviewPage extends React.Component {
+  state = { components: [], fragments: wagtailBaseFragments.source };
 
-        if (fragmentFiles.length) {
-            fragmentFiles.map(file => {
-                const module = require(`../../${file.slice(2)}`)
-                Object.keys(module).map(exportKey => {
-                    const exportObj = module[exportKey]
-                    if (typeof exportObj.source == 'string') {
-                        fragments += exportObj.source
-                    }
+  async componentDidMount() {
+    await this.fetchFragments()
+    await this.fetchComponents()
+  }
+
+  fetchFragments = () => {
+    const { fragmentFiles } = this.props.pageContext;
+
+    return Promise.all(
+      fragmentFiles.map(file => {
+        import("../../src/" + file)
+          .then(mod => {
+            Object.keys(mod).map(exportKey => {
+              const exportObj = mod[exportKey];
+              if (typeof exportObj.source == "string") {
+                this.setState({
+                  fragments: this.state.fragments += exportObj.source
                 })
-            })
-        }
+              }
+            });
+          })
+          .catch(e => null);
+      })
+    )
+  }
 
-        Object.keys(pageMap).map(contentType => {
-            const componentFile = require(`../../${pageMap[contentType].slice(2)}`)
-            components[contentType.toLowerCase()] = withPreview(
-                componentFile.default, 
-                componentFile.query,
-                fragments
-            )
+  fetchComponents = () => {
+    const { pageMap } = this.props.pageContext;
+    
+    return Promise.all(
+      Object.keys(pageMap).map(contentType => {
+          import("../../src/" + pageMap[contentType])
+            .then(componentFile => {
+              this.setState({
+                components: {
+                  ...this.state.components,
+                  [contentType.toLowerCase()]: withPreview(
+                    componentFile.default,
+                    componentFile.query,
+                    this.state.fragments
+                  )
+                }
+              });
+            })
+            .catch(e => null);
         })
-       
-        const { content_type } = decodePreviewUrl()
-        if (content_type) {
-            const Component = components[content_type.toLowerCase()]
-            if (Component) {
-                return <Component />
-            }
-        }
-        return null
-    } 
-    return null
+    )
+  };
+
+  render() {
+    const { content_type } = decodePreviewUrl();
+    if (content_type) {
+      const Component = this.state.components[content_type.toLowerCase()];
+      if (Component) {
+        return <Component />;
+      }
+    }
+    return null;
+  }
 }
 
-export default PreviewPage
+export default PreviewPage;

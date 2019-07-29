0a26e2a50029828436bcde7c6f75d1bb310260d7
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-07-29 13:04:26 +0000

    fix: gatsby babel plugin options

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
+
+
+
 const { prepareOptions } = require(`gatsby/dist/utils/babel-loader-helpers`)
 
-exports.prepareOptions = (babel) => {
-  const items = prepareOptions(babel)
+exports.prepareOptions = (babel, options) => {
+  const items = prepareOptions(babel, options)
 
   if (items.length > 2) {
     items[3].splice(
